6aaa866ee8201f9afd369a4e219e6b99d36094c8
Aman-dlt <83169263+Aman-dlt@users.noreply.github.com>
2021-05-13 19:07:05 +0530

    input output processing done

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-/*var username = prompt("gimme a username");
-alert("helo" +username);*/
-console.log("hello form another script")
+ var btnTranslate = document.querySelector("#btn-translate");
+ var txtArea = document.querySelector("#txtinput");
+ var OutPutdiv = document.querySelector("#outputbox");
+ 
+function clickHandler()
+  {
+      OutPutdiv.innerText = "ajdnonooo" + txtArea.value;
+        
+};
+ 
+
+btnTranslate.addEventListener("click", clickHandler)
